ce2006cc5726ac393088efb87f9e5ff6ea3fd944
HaniiHaz03 <nurul_24000182@utp.edu.my>
2024-06-02 16:00:21 +0800

    Update Clients.java

--- Clients.java
+++ Clients.java
@@ -1,1 +1,48 @@
+//Name: Zulaikha binti Mohd Azhar
+//ID: 24000918
+//Program: Computer Science
 
+public class Main {
+    public static void main (String[]args) {
+        
+        Clients women = new Clients(); //object name is women
+        
+        //calling the actions
+        women.addClients();
+        women.removeClients();
+        women.printClients();
+        women.findClients();
+        
+        Clients men = new Clients(); //object name is men
+        
+        //calling the actions
+        men.addClients();
+        men.removeClients();
+        men.printClients();
+        men.findClients();
+    }
+}
+/*CLASS FOR CLIENTS*/
+public class Clients {
+    private String clients; //The code is only accessible within the declared class
+    
+    //action to add clients data
+    public void addClients() { //void indicates method does not return any value
+        
+    }
+    
+    //action to remove clients data
+    public void removeClients() {
+        
+    }
+    
+    //action to print clients data
+    public void printClients() {
+        
+    }
+    
+    //action to find clients data
+    public void findClients() {
+        
+    }
+}
