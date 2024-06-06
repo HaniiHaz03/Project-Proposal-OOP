c94724f9ab71cc922b96151b9f15734731de5a22
HaniiHaz03 <nurul_24000182@utp.edu.my>
2024-06-06 19:49:27 +0800

    Update Main.java

--- Saloon-Reservation-Sytem/Main.java
+++ Saloon-Reservation-Sytem/Main.java
@@ -1,1 +1,21 @@
+import java.util.ArrayList;
 
+public class Main
+{
+	public static void main(String[] args) {
+		
+		Stylist stylist1 = new Stylist("Ally", 45.0);
+		Stylist stylist2 = new Stylist("Suzanne", 67.0);
+
+    if(stylist1.getName()==reservation.getHairStylistName()){
+		  stylist1.getReservations().add(reservation1);
+    }
+    else{
+      stylis11.getReservations().add(reservation2);
+    }
+    
+		stylist1.displayStylistInfo();
+		stylist2.displayStylistInfo();
+		
+	}
+}
