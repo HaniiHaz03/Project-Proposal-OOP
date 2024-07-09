701cc9bbfbf374e4e168d43f1d964098143cdc30
HaniiHaz03 <nurul_24000182@utp.edu.my>
2024-07-09 19:38:49 +0800

    Update Reservations.java

--- JavaFXGUI/Reservations.java
+++ JavaFXGUI/Reservations.java
@@ -16,6 +16,25 @@ public class Reservations {
         reservations.add(reservation);
     }
 
+    public void removeReservationByName(String clientName) {
+        reservations.removeIf(reservation -> reservation.getClientName().equals(clientName));
+    }
+
+    public Reservation findReservationByName(String clientName) {
+        for (Reservation reservation : reservations) {
+            if (reservation.getClientName().equals(clientName)) {
+                return reservation;
+            }
+        }
+        return null;
+    }
+
+    public void printReservations() {
+        for (Reservation reservation : reservations) {
+            System.out.println(reservation);
+        }
+    }
+
     public List<Reservation> getReservations() {
         return new ArrayList<>(reservations);
     }
