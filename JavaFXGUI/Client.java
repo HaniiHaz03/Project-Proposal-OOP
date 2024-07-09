b92475a20c3248439e09de0996969a278977abaa
HaniiHaz03 <nurul_24000182@utp.edu.my>
2024-07-09 18:30:47 +0800

    Update Client.java

--- JavaFXGUI/Client.java
+++ JavaFXGUI/Client.java
@@ -2,3 +2,35 @@
 //Name: Sara Eudora Binti Said
 //ID: 24000574
 //Program: Computer Science
+public class Client
+{
+    protected String name;
+    protected String phoneNumber;
+    protected String email;
+    
+    public Client(String name, String phoneNumber, String email)
+    {
+         this.name = name;
+         this.phoneNumber = phoneNumber;
+         this.email = email;
+    }
+    
+    // Getter methods to access client information
+  public String getName() {
+    return name;
+  }
+
+  public String getPhoneNumber() {
+    return phoneNumber;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+  
+  @Override
+    public String toString() {
+        return "Name: " + getName() + "  " + "Phone Number:" + getPhoneNumber() + "  " + "Email:" + getEmail() + " ";
+    }
+    
+}
