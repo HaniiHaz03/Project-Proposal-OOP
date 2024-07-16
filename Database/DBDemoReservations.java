acf587e6eb6e15e4a3d4ce40659138b9480f1b93
HaniiHaz03 <nurul_24000182@utp.edu.my>
2024-07-16 23:56:10 +0800

    Update DBDemoReservations.java

--- Database/DBDemoReservations.java
+++ Database/DBDemoReservations.java
@@ -1,19 +1,16 @@
 public class DBDemo
 {
-
-    /**
-     * Constructor for objects of class DBDemo
-     */
+    // instance variables - replace the example below with your own
+   
     public DBDemo()
     {
-        
-    }
-
-    public static void main(String[] args) {
-        
-        SqlDB db = new SqlDB("sql12.freesqldatabase.com", "sql12720141", "wUGaWRv62A");
+     
+    }  
+    
+   public static void main(String[] args) {
+        SqlDB db = new SqlDB("sql12.freesqldatabase.com", "sql12720115", "iEmWcdsfk2");
         db.init();
+        //db.insert("John Doe", "1234567890", "john@example.com", "2024-07-16", "14:00", "Haircut", "Jane Smith");
         db.query();
     }
-
 }
