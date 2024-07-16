13a6e4cca94b4a44244b5113183d7070e3db033a
HaniiHaz03 <nurul_24000182@utp.edu.my>
2024-07-16 21:41:54 +0800

    Update DBDemoClients.java

--- Database/DBDemo.java
+++ Database/DBDemo.java
@@ -1,1 +1,23 @@
 
+
+
+public class DBDemo
+{
+
+    /**
+     * Constructor for objects of class DBDemo
+     */
+    public DBDemo()
+    {
+        
+    }
+
+    public static void main(String[] args) {
+        
+        SqlDB db = new SqlDB("sql12.freesqldatabase.com", "sql12719044", "PQUSjvXBEP");
+        db.init();
+        //db.insert("Zulaikha", "019 1234567", "zulaikha@gmail.com");
+        db.query();
+    }
+
+}
